a840547084774a249eae7a4b6cec106c34861ed4
David Lens <davidlens51@gmail.com>
2021-03-11 22:59:50 -0800

    EDA partially done, multiple errors needed to be fixed

--- analysis/Scripts/project_files.py
+++ analysis/Scripts/project_files.py
@@ -9,6 +9,9 @@
 
 import pandas as pd
 import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 #line 30
 
@@ -16,7 +19,23 @@ def load_and_process(url_or_path_to_csv_file):
     
     
     df = (pd.read_csv(url_or_path_to_csv_file)
-          .drop(columns = ['DIVERTED','CANCELLED', 'CANCELLATION_REASON', 'AIR_SYSTEM_DELAY', 'SECURITY_DELAY','AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY','WEATHER_DELAY'])
+          .drop(columns = ['DIVERTED','CANCELLED', 'CANCELLATION_REASON', 'AIR_SYSTEM_DELAY', 'SECURITY_DELAY','AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY','WEATHER_DELAY','Unamed: 31'])
           .dropna(axis = 0)
           .rename(columns = {"WHEELS_OFF" : "GATE_TO_TAKEOFF"}))
     return df
+
+
+def displot(df):
+    sns.displot(df['DAY'], kde=False, bins=31).set_title('number of flights on days during the month')
+    
+    
+def displot2(df):
+    sns.displot(df['AIRLINE'], kde=False, bins=13).set_title('amount of flights per airline')
+    
+    
+def displot3(df):
+    sns.displot(df['ORIGIN_AIRPORT'], kde=False, bins=322).set_title('departing flights')
+    
+    
+def barplot(df):
+    sns.barplot(y = 'DELAYED_OR_NOT', data =df).set_title('number of flights delayed versus on time')
